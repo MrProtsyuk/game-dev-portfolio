5bf98155ce63a809ac0cefc2d186509aa955277d
Mark Protsyuk <marcprotsyuk@gmail.com>
2023-03-20 23:38:45 -0700

    added emailjs

--- app.js
+++ app.js
@@ -11,3 +11,24 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+function sendMail() {
+    var params = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      message: document.getElementById("message").value,
+    };
+  
+    const serviceID = "service_zflqw0p";
+    const templateID = "template_fka4a3f";
+  
+      emailjs.send(serviceID, templateID, params)
+      .then(res=>{
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+          console.log(res);
+      })
+      .catch(err=>console.log(err));
+    }
